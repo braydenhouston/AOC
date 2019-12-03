2ad054175ced15baf1d9c15eae880780a52c081f
Brayden Houston <braydenjhouston@gmail.com>
2019-12-03 00:36:45 -0700

    Day 3 Part 2
    Still kinda slow......but it works ;}

--- Day3/main.py
+++ Day3/main.py
@@ -22,8 +22,8 @@ wire1 = lines[0].rstrip().split(",")
 wire2 = lines[1].rstrip().split(",")
 
 # Test Cases
-# wire1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(",")
-# wire2 = "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
+#wire1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(",")
+#wire2 = "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
 
 cords = [0, 0]
 
@@ -46,17 +46,28 @@ for dir in wire2:
      for i in range(step):
           path2.append(updateCord(cords, cmd)[:])
 
-print("Finding intersection")
+print("Finding intersection of paths")
+
 intercept = intersection(path1, path2)
 
-shortest = 0
+shortDist = 0
 shortcord = []
+shortSteps = 0
+fastcord = []
 
-print("Finding distance")
+print("Finding shortest distance coords")
 for cord in intercept:
      s = calcDistance(cord)
-     if  s < shortest or shortest == 0:
-          shortest = s
+     if  s < shortDist or shortDist == 0:
+          shortDist = s
           shortcord = cord
 
-print("Shortest distance of ", shortest, " is at ", shortcord)
+print("Finding fastest coords")
+for cord in intercept:
+     s = path1.index(cord) + path2.index(cord) + 2 # +2 to account for both lists 0 index
+     if s < shortSteps or shortSteps == 0:
+          shortSteps = s
+          fastcord = cord
+
+print("Shortest distance of ", shortDist, " is at ", shortcord)
+print("Fastest in ", shortSteps, " steps is at ", fastcord)
