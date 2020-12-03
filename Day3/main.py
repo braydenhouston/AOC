1c603087dd5e6d7040e08002568976cb8510f103
Brayden Houston <braydenjhouston@gmail.com>
2020-12-02 23:11:50 -0700

    AOC2020 Day 3 Part 2

--- Day3/main.py
+++ Day3/main.py
@@ -1,3 +1,18 @@
+def sled(right, down):
+    col = 0
+    hits = 0
+    for row in range(0, len(trees), down):
+        if col >= len(trees[row]):
+            col -= len(trees[row])
+
+        if trees[row][col] == "#":
+            hits += 1
+
+        col += right
+    # print("Right: {} Down: {} Hits: {}".format(right, down, hits))
+    return hits
+
+
 with open('input.txt') as f:
     lines = f.readlines()
 
@@ -5,24 +20,14 @@ trees = [[]]*len(lines)
 for pos, line in enumerate(lines, start=0):
     trees[pos] = [char for char in line.strip()]
 
-col = 0
-hits = 0
+print("Part 1: {} hits".format(sled(3, 1)))
 
-for row in range(0, len(trees)):
-    if col >= len(trees[row]):
-        col -= len(trees[row])
-
-    if trees[row][col] == "#":
-        hits += 1
-        trees[row][col] = "X"
-    else:
-        trees[row][col] = "O"
-
-    col += 3
-
-for row in range(0, len(trees)):
-    print(trees[row])
-
-print("Part 1: {}".format(hits))
+print("Part 2: {}".format(
+    sled(1, 1) *
+    sled(3, 1) *
+    sled(5, 1) *
+    sled(7, 1) *
+    sled(1, 2)
+))
 
 
