705e2056b7a394e8140486fe0ad65eb1f3458576
Brayden Houston <braydenjhouston@gmail.com>
2020-12-06 17:34:57 -0700

    AOC2020 Day 6

--- Day6/main.py
+++ Day6/main.py
@@ -2,18 +2,27 @@
 
 file = open('input.txt')
 lines = file.read().split("\n\n")
-
+file.close()
 
 s = 0
 for line in lines:
     letters = {}
-
     for l in line:
         for c in l.replace("\n", ""):
             letters[c] = 0
-
     s += len(letters.keys())
+print("Part 1 {}".format(s))
 
 
-print(s)
+s = 0
+for groups in lines:
+    letters = {}
+    person = groups.split("\n")
+    for answers in person:
+        for answer in answers:
+            letters[answer] = letters.get(answer, 0) + 1
+    for a in letters.keys():
+        if letters[a] == len(person):
+            s += 1
+print("Part 2: {}".format(s))
 
