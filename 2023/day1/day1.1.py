df121b6df5c51bae9d9ac45b43ae2190b13108fe
Brayden Houston <braydenjhouston@gmail.com>
2023-12-01 15:15:32 -0600

    Day 1.1

---
+++ 2023/day1/day1.1.py
@@ -0,0 +1,19 @@
+import os
+
+def only_nums(seq):
+    seq_type= type(seq)
+    return seq_type().join(filter(seq_type.isdigit, seq))
+
+
+ll = [x for x in open(os.path.join(os.path.dirname(__file__),'input.txt')).read().strip().split('\n')]
+sum = 0
+
+for l in ll: 
+    n = only_nums(l)
+    print("First {} Last: {}".format(n[0], n[-1]))
+    sum += int(n[0] + n[-1])
+    
+print("Sum {}".format(sum))
+
+# print(f"Part 1: {cals[-1]}")
+# print(f"Part 2: {sum(cals[-3::])}")
